9d39a9b9c665ea1828c7a6e7d841d7e0e03a892b
Romain <romain.corfmatpro@gmail.com>
2025-01-16 12:00:07 +0800

    Implement RateLiming for authentification

--- lib/actions/auth.ts
+++ lib/actions/auth.ts
@@ -2,16 +2,27 @@
 
 import { hash } from "bcryptjs";
 import { eq } from "drizzle-orm";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
 import { signIn } from "@/auth";
 import { db } from "@/database/drizzle";
 import { users } from "@/database/schema";
 import { AuthCredentials } from "@/types";
 
+import ratelimit from "../ratelimit";
+
 export const signInWithCredentials = async (
   params: Pick<AuthCredentials, "email" | "password">
 ) => {
   const { email, password } = params;
+
+  const ip =
+    (await headers()).get("x-forwarded-for") || "127.0.0.1";
+
+  const { success } = await ratelimit.limit(ip);
+
+  if (!success) return redirect("/too-fast");
 
   try {
     const result = await signIn("credentials", {
@@ -32,6 +43,13 @@ export const signInWithCredentials = async (
 };
 
 export const signUp = async (params: AuthCredentials) => {
+  const ip =
+    (await headers()).get("x-forwarded-for") || "127.0.0.1";
+
+  const { success } = await ratelimit.limit(ip);
+
+  if (!success) return redirect("/too-fast");
+
   const {
     fullName,
     email,
---
+++ lib/ratelimit.ts
@@ -0,0 +1,29 @@
+import { Ratelimit } from "@upstash/ratelimit"; // for deno: see above
+
+import redis from "@/database/redis";
+
+// Create a new ratelimiter, that allows 10 requests per 10 seconds
+const ratelimit = new Ratelimit({
+  redis,
+  limiter: Ratelimit.fixedWindow(5, "1m"),
+  analytics: true,
+  /**
+   * Optional prefix for the keys used in redis. This is useful if you want to share a redis
+   * instance with other applications and want to avoid key collisions. The default prefix is
+   * "@upstash/ratelimit"
+   */
+  prefix: "@upstash/ratelimit",
+});
+
+export default ratelimit;
+
+// Use a constant string to limit all requests with a single ratelimit
+// Or use a userID, apiKey or ip address for individual limits.
+// const identifier = "api";
+// const { success } = await ratelimit.limit(identifier);
+
+// if (!success) {
+//   return "Unable to process at this time";
+// }
+// doExpensiveCalculation();
+// return "Here you go!";
